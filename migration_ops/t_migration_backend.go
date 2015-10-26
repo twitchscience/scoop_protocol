c037719c0f85c94f041742b9b5bd3c1f20dd3f17
Dhruv Garg <dgarg@twitch.tv>
2015-10-26 15:35:24 -0700

    Update function headers and set up overall structure

--- migration_ops/t_migration_backend.go
+++
@@ -1,7 +0,0 @@
-package migration_ops
-
-import "github.com/twitchscience/scoop_protocol/schema_storer"
-
-type TestMigrationOps struct {
-	newMigration Migration
-}
--- migration_validator/migration_validator.go
+++ migration_validator/migration_validator.go
@@ -1,5 +1,5 @@
 package migration_validator
 
 type MigrationValidator interface {
-	Validate()
+	Validate() error
 }
